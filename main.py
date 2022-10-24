8802cff00c915fe4a816e9bf9f84668ecfff6602
rauch <rauch@rising-systems.de>
2022-10-24 17:20:10 +0200

    Update api

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from flask import Flask, json
+from flask import Flask, json, jsonify
 
-companies = [{"id": 1, "name": "Company One"},
-             {"id": 2, "name": "Company Two"}]
+meals = [{"name": "Spaghetti Carbonara", "price": 7.99, "origin": "Italy", "type": "pasta", "emoji": "🍝", "ingredients": ["egg", "bacon", "parmesan", "spaghetti"], "calories": 500},
+         {"name": "Chicken Tikka Masala", "price": 8.99, "origin": "India", "type": "curry",
+             "emoji": "🍛", "ingredients": ["chicken", "spices", "tomato", "cream"], "calories": 600},
+         {"name": "Burger", "price": 5.99, "origin": "Germany", "type": "fast food",
+             "emoji": "🍔", "ingredients": ["beef", "bun", "cheese", "lettuce"], "calories": 700},
+         {"name": "Pancakes", "price": 6.99, "origin": "France", "type": "breakfast",
+             "emoji": "🥞", "ingredients": ["eggs", "flour", "sugar", "butter"], "calories": 800},
+         {"name": "Pizza", "price": 6.99, "origin": "Italy", "type": "fast food", "emoji": "🍕",
+             "ingredients": ["dough", "cheese", "tomato", "pepperoni"], "calories": 900},
+         {"name": "Lasagna", "price": 7.99, "origin": "Italy", "type": "pasta", "emoji": "🍝",
+             "ingredients": ["beef", "cheese", "spices", "tomato"], "calories": 500},
+         {"name": "Risotto", "price": 8.99, "origin": "Italy", "type": "pasta", "emoji": "🍝",
+             "ingredients": ["rice", "cheese", "spices", "tomato"], "calories": 600},
+         {"name": "Tiramisu", "price": 5.99, "origin": "Italy", "type": "dessert", "emoji": "🍰",
+             "ingredients": ["eggs", "sugar", "coffee", "sponge cake "], "calories": 700},
+         {"name": "Spaghetti Bolognese", "price": 6.99, "origin": "Italy", "type": "pasta",
+             "emoji": "🍝", "ingredients": ["beef", "spices", "tomato", "spaghetti"], "calories": 800},
+         {"name": "Pasta", "price": 6.99, "origin": "Italy", "type": "pasta", "emoji": "🍝", "ingredients": ["spaghetti", "tomato", "cheese", "basil"], "calories": 900}]
+
 
 api = Flask(__name__)
 
@@ -9,7 +26,7 @@ api = Flask(__name__)
 @api.route('/', methods=['GET'])
 def get_data():
     print("get all")
-    return json.dumps(companies)
+    return jsonify(meals)
 
 
 if __name__ == '__main__':
